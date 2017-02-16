fa98bbfa3a63d7761172d9b53aed79bc2d94766c
Prasanth J <prasanth.j@tpv-tech.com>
2017-02-16 12:06:52 +0530

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SimpleNodeSocket
+
+A simple NodeJS server using [socket.io](http://socket.io)..
+
+The server sends a simple message to make sure the client is connected and client send message to server using a simple UI, which displays the message received in the console on server end.
+
+Clone and run using **node server**
+
+Visit http://localhost:9000/
--- server.js
+++ server.js
@@ -4,16 +4,17 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
 
-app.get('/', function(req, res, next){
-  fs.readStream(__dirname + '/index.html', function(err, data){
-    res.sendfile(__dirname + '/index.html');
-    return next();
-  })
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.emit('msg', {'text': 'Welcome to socket programming <3 NodeJS'});
-  socket.on('response', function(msg){
-    console.log('Message from client ' + msg);
+  socket.on('response', function(data){
+    console.log('Message from client ' + data.msg);
   })
 })
+
+server.listen(9000, function(){
+  console.log('Server started.');
+});
